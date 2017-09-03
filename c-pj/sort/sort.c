be63de2f39377f3c1d06a387e4d3ef601565555d
rabitdash <rabitdash@outlook.com>
2017-09-03 13:14:01 +0800

    update:

--- c-pj/sort/sort.c
+++ c-pj/sort/sort.c
@@ -19,13 +19,13 @@ int* insert_sort(int* a, int len)
 	for(j = 1; j < len; j++)
 	{
 		key = a[j];
-		//Insert a[j] into the sorted sequence a[i..j-1]
+		// Insert a[j] into the sorted sequence a[i..j-1]
 		i = j - 1;
 		while( i >= 0 && a[i] < key)
 		{
 			a[i + 1] = a[i];
 			i--;
-		}
+		}// Move elements
 		a[i+1] = key;
 	}
 	return a;
