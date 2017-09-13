0b7ffd3b0ec248fb5b44542e654f28a1bc1f0d00
rabitdash <rabitdash@outlook.com>
2017-09-13 20:05:55 +0800

    update

---
+++ cpp-pj/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double amount = 22.0/7;
+    cout<<amount<<endl;
+    cout<<setprecision(0)<<amount<<endl;
+    return 0;
+}
