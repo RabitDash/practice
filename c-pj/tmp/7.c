65c26abb2dd805267e1887a36479a81f9b9b8e9d
rabitdash <rabitdash@outlook.com>
2017-09-29 21:22:45 +0800

    update

--- c-pj/tmp/7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int fuck()
-{
-	static int i;
-	i = 12;
-	printf("\nfuck:%d", i);
-	return 0;
-}
--- c-pj/tmp/8.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-static int fuck()
-{
-	printf("\nshit!");
-}
