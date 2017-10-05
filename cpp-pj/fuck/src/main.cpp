5a655d098207afa53570cbe874e2febb12058d8f
rabitdash <rabitdash@outlook.com>
2017-10-05 17:54:48 +0800

    update

--- cpp-pj/fuck/src/main.cpp
+++ cpp-pj/fuck/src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
+class fuck
+{
+	public:
+		int i;
+		int b;
+		fuck()
+		{
+			cout << "instance created!";
+		}
+		~fuck()
+		{
+			cout << "instance destroyed!";
+		}
+};
+
 int main()
 {
-	string a;
-	cin >> a;
-	cout << a << endl;
+	fuck you;
 	return 0;
 }
