316f34fdef4713b1507fd713f14a312b4e73abf0
rabitdash <rabitdash@outlook.com>
2017-10-22 12:41:23 +0800

    rm c-interactice-console/*

--- python-pj/c-interactive-console/init.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "stdfx.h"
-
-int main()
--- python-pj/c-interactive-console/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "stdfx.h"
-int main()
-{
-printf("fuck!");
-return 0;
-}
