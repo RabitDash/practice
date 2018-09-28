aece463b7cbe3d517a467e8971d5f341b3e273f9
rabitdash <rabitdash@outlook.com>
2018-09-28 14:11:30 +0800

    update

---
+++ cpp-pj/data_structure/3/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+	int elem;
+	struct Node* next;
+} Node;
+
+Node* InitList(){
+	Node* head;
+	head = (Node*)malloc(sizeof(Node));
+	head->next = NULL;
+	return head;
+}
+
+void Append(Node* head, int elem)
+{
+	Node* p = head;
+	while(p->next != NULL)
+	{
+		p = p->next;
+	}
+	p->next = (Node*)malloc(sizeof(Node));
+	p->next->elem = elem;
+	p->next->next = NULL;
+}
+
+void Reverse(Node* head)
+{
+	Node* tmphead;
+}
+int main()
+{
+	Node* head;
+	head=InitList();
+	int i;
+	for(i = 0; i < 20; i++)
+	{
+		Append(head, i);
+	}
+	while(head->next != NULL)
+	{
+		printf("%d ", head->elem);
+		head = head->next;
+	}
+	return 0;
+}
