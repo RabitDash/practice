12396bf88c90f9af9a804a0900ee501b2825d098
rabitdash <rabitdash@outlook.com>
2018-10-23 21:36:47 +0800

    update

--- r-pj/733-4-luyue.R
+++ r-pj/733-4-luyue.R
@@ -1,39 +1,58 @@
+# 一、基本操作
+
+## 1
 x<-seq(-4*pi,4*pi,0.1)
 y<-sin(x)
-plot(x,y,type="b")
+plot(x,y,type="o")
 
+## 2
 t<-rnorm(40)
-t
+View(t)
 t<-sort(t,decreasing=T)
-t
 which.max(t)
 max(t)
 mean(t)
 median(t)
 boxplot(t)
 
+## 3
 A<-matrix(c(1,3,3,3,3,2,3,3,3,3,3,3,3,3,3,4),ncol=4,byrow=T)
 det(A)
 nrow(A)
 ncol(A)
 B<-t(A)
-B
 A%*%B
 
+## 4
 C<-diag(1:4)
+View(C)
 
+## 5
 b<-1:4
 x<-solve(A,b)
 View(x)
 
+## 6
 iris.1<-iris[iris[1]>5,]
-iris.1
+View(iris.1)
 
+
+# 二、数据题目
+
+## 1
 white<-read.csv("white.csv", sep=";")
 typeof(white)
+nrow(white)
+
+## 2
 unique(white["quality"])
+
+## 3
 colnames(white)[12]<-"yf"
-box(white[1],white["yf"])
+boxplot(white[1],white["yf"])
+
+
+# 三
 f<-function(x){
 	if(x > 1)
 	{
@@ -46,6 +65,9 @@ f<-function(x){
 }
 f(1)+2*f(3)
 x<-seq(-5,5,0.1)
-plot(x,sapply(x, f),main = "the plot of f",type = "l",col="red")
+plot(x,sapply(x, f),main = "the plot of f",type = "o",col="red")
+
+
+# 四
 matrix(sapply(1:10000, function(i){0.4^abs(i%%100-i%/%100-1)}),nrow=100)
 
